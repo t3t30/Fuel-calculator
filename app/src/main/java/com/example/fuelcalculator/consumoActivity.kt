c2460e104b841c007be60d497b5a7164b75f5545
t3t30 <mateusbie6@gmail.com>
2024-12-03 19:55:58 -0300

    Logica pronta e conexão entre paginas tambem. falta melhorar a User Interface

--- app/src/main/java/com/example/fuelcalculator/consumoActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/consumoActivity.kt
@@ -1,11 +1,15 @@
 package com.example.fuelcalculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-const val  KEY_PREC = "resultpreco.KEY_PRECO"
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
 const val KEY_DISTANCIA = "resultdistancia.KEY_DISTANCIA"
 class consumoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +17,29 @@ class consumoActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_consumo)
 
-        val preco = intent.getFloatExtra(KEY_PRCO, 0f)
-        val
+        val distanciaporpreco = intent.getFloatExtra(KEY_DISTANCIA, 0f)
+        val txtconsumo = findViewById<TextInputEditText>(R.id.txtconsumo)
+        val btnconsumo = findViewById<Button>(R.id.btnconsumo)
+
+        btnconsumo.setOnClickListener{
+            val strconsumo: String = txtconsumo.text.toString()
+            if (strconsumo == ""){
+                Snackbar.make(
+                    txtconsumo,
+                    "Preencha um consumo válido!",
+                    Snackbar.LENGTH_LONG ) }
+            else{
+                val consumo = strconsumo.toFloat()
+                val result = distanciaporpreco/consumo
+
+                val intent = Intent (this, resultActivity::class.java)
+                intent.putExtra( KEY_RESULT, result )
+                startActivity(intent)
+
+
+            }
+
+        }
 
 
 
--- app/src/main/java/com/example/fuelcalculator/resultActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/resultActivity.kt
@@ -1,20 +1,25 @@
 package com.example.fuelcalculator
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+const val KEY_RESULT = "resultresult.KEY_RESULT"
 
 class resultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val result = intent.getFloatExtra(KEY_RESULT, 0f)
+        val txtresult = findViewById<TextView>(R.id.txtresult)
+        val resultformat = String.format("%.2f", txtresult)
+        "R$ ${resultformat}".also { txtresult.text = it }
+
+
     }
 }
