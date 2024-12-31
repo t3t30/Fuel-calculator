0a07a64ee59a99600b6e8e245c9321ecc7fa9554
t3t30 <mateusbie6@gmail.com>
2024-12-31 17:05:33 -0300

    result page almost ready

--- app/src/main/java/com/example/fuelcalculator/resultActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/resultActivity.kt
@@ -19,11 +19,23 @@ class resultActivity : AppCompatActivity() {
         val distancia = intent.getFloatExtra("KEY DISTANCIA", 0f)
         val consumo = intent.getFloatExtra("KEY CONSUMO", 0f)
         val txtresult = findViewById<TextView>(R.id.txtresult)
+        val txtpreco = findViewById<TextView>(R.id.txttpreco)
+        val txtdistancia = findViewById<TextView>(R.id.txttdistancia)
+        val txtconsumo = findViewById<TextView>(R.id.txttconsumo)
 
         val total = distancia/consumo * preco
 
         val resultformat = String.format("R$ %.2f", total)
         txtresult.text = resultformat
+
+        val precoformat = String.format("Preço da gasolina: %.2f", preco)
+        txtpreco.text = precoformat
+
+        val distanciaformat = String.format("Distância: %.2f" , distancia , "km %.2f")
+        txtdistancia.text = distanciaformat
+
+        val consumoformat = String.format("Consumo do carro: %.2f", consumo, "%.2f km/l")
+        txtconsumo.text = consumoformat
 
 
 
